933862f0116a0dbc9f36cca0a82d6b01c4377891
_Salmon2 <57042065+punipuni-3730@users.noreply.github.com>
2022-04-10 01:59:18 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//メモ
+// S = 路線名の差異、比較元　もし乗換駅が出たら、Sは書き換わって乗換先路線が比較元になる。
+// U = 乗換駅･その路線名。damta.main[x]の形で入ってる。
+// i = 乗換駅検索の繰り返し回数。
+// R = 乗車駅
+// G = 下車駅
+
+function requestbutton() {
+    let from = document.getElementById("from").value;
+    let to = document.getElementById("to").value;
+    fetch(`https://script.google.com/macros/s/AKfycbxYA8zo16so3zlystt-1vTK8GbpRANUZPOdphESCQy2GmEAlQpj_POQtCR8bdaOnpM/exec?from=${from}&to=${to}`)
+        .then(response => response.json())
+        .then(data => {
+
+            let search = data.main[0][1];//出発駅代入
+            let result_data_id = document.getElementById('result_data');//<div id="result_data">を取得
+
+            result_data_id.innerHTML = "";//<div id="result_data">内を何も書いていない状態にする
+
+            result_data_id.insertAdjacentHTML('beforeend', `<h1>${data.main[0][0]}</h1><p>の${data.main[0][1]}から乗車</p>`);//乗車する場所表示
+
+            for (let loop = 0; loop < data.main.length; loop++) {
+                if (data.main[loop][1] != search) {//もし乗換する場所を見つけたら乗換表示の要素追加
+                    search = data.main[loop][1];
+                    result_data_id.insertAdjacentHTML('beforeend', `<h1>${data.main[loop-1][0]}</h1><p>で${data.main[loop][1]}に乗り換え</p>`);
+                }
+            }
+
+            result_data_id.insertAdjacentHTML('beforeend', `<h1>${data.main.slice(-1)[0][0]}</h1><p>で下車</p>`);//降りる場所表示
+
+            result_data_id.insertAdjacentHTML('beforeend', `<p>移動距離は${data.length}ブロックです。`);//移動ブロック表示
+
+            console.log(data.main);
+        });
+}
