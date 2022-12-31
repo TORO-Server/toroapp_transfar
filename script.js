fa4d900fb70148c5d4e894c014d79837d29b1565
malken21 <Malken_Marumasa@outlook.jp>
2023-01-01 03:17:30 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,9 @@
 
 registSW();
 
+// 経路検索API の URL
+const url = "https://script.google.com/macros/s/AKfycbxYA8zo16so3zlystt-1vTK8GbpRANUZPOdphESCQy2GmEAlQpj_POQtCR8bdaOnpM/exec";
+
 function registSW() {
 
   // Service Worker 対応ブラウザの場合、スコープに基づいてService Worker を登録する
@@ -23,34 +26,74 @@ function registSW() {
     });
   }
 }
-function requestbutton() {
-    let from = document.getElementById("from").value;
-    let to = document.getElementById("to").value;
-    fetch(`https://script.google.com/macros/s/AKfycbxYA8zo16so3zlystt-1vTK8GbpRANUZPOdphESCQy2GmEAlQpj_POQtCR8bdaOnpM/exec?from=${from}&to=${to}`)
-        .then(response => response.json())
-        .then(data => {
 
-            let search = data.main[0][1];//出発駅代入
-            let result_data_id = document.getElementById('result_data');//<div id="result_data">を取得
-
-            result_data_id.innerHTML = "";//<div id="result_data">内を何も書いていない状態にする
-
-            result_data_id.insertAdjacentHTML('beforeend', `<div class="sta"><div class="station"><br></div><h1>${data.main[0][0]}</h1></div><div class="line"><div class="connect"><br></div><p>${data.main[0][1]}</p></div>`);//乗車する場所表示
-
-            for (let loop = 0; loop < data.main.length; loop++) {
-                if (data.main[loop][1] != search) {//もし乗換する場所を見つけたら乗換表示の要素追加
-                    search = data.main[loop][1];
-                    result_data_id.insertAdjacentHTML('beforeend', `<div class="sta"><div class="trans"><br></div><h1>${data.main[loop-1][0]}</h1></div><div class="line"><div class="connect"><br></div><p>${data.main[loop][1]}</p></div>`);
-                }
-            }
-
-            result_data_id.insertAdjacentHTML('beforeend', `<div class="sta"><div class="station"><br></div><h1>${data.main.slice(-1)[0][0]}</h1></div>`);//降りる場所表示
-
-            let min = ~~(data.length/60);
-            let sec = (data.length%60);
-
-            result_data_id.insertAdjacentHTML('beforeend', `<br><p>移動距離は${min}分${sec}秒です。`);//移動ブロック表示
-
-            console.log(data.main);
-        });
+// ServiceWorker登録：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function (registration) {
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function (err) {
+    console.log('ServiceWorker registration failed: ', err);
+  });
 }
+
+//---------- 検索 & 検索結果表示処理 ----------//
+async function requestbutton() {
+  let from = document.getElementById("from").value;
+  let to = document.getElementById("to").value;
+
+  // Get リクエスト
+  const data = await get(`${url}?from=${from}&to=${to}`);
+
+  let search = data.main[0][1];//出発駅代入
+  let result_data_id = document.getElementById('result_data');//<div id="result_data">を取得
+
+  result_data_id.innerHTML = "";//<div id="result_data">内を何も書いていない状態にする
+
+  result_data_id.insertAdjacentHTML('beforeend', `<div class="sta"><div class="station"><br></div><h1>${data.main[0][0]}</h1></div><div class="line"><div class="connect"><br></div><p>${data.main[0][1]}</p></div>`);//乗車する場所表示
+
+  for (let loop = 0; loop < data.main.length; loop++) {
+    if (data.main[loop][1] != search) {//もし乗換する場所を見つけたら乗換表示の要素追加
+      search = data.main[loop][1];
+      result_data_id.insertAdjacentHTML('beforeend', `<div class="sta"><div class="trans"><br></div><h1>${data.main[loop - 1][0]}</h1></div><div class="line"><div class="connect"><br></div><p>${data.main[loop][1]}</p></div>`);
+    }
+  }
+
+  result_data_id.insertAdjacentHTML('beforeend', `<div class="sta"><div class="station"><br></div><h1>${data.main.slice(-1)[0][0]}</h1></div>`);//降りる場所表示
+
+  let min = ~~(data.length / 60);
+  let sec = (data.length % 60);
+
+  result_data_id.insertAdjacentHTML('beforeend', `<br><p>移動距離は${min}分${sec}秒です。`);//移動ブロック表示
+
+  console.log(data.main);
+}
+//---------- 検索 & 検索結果表示処理 ----------//
+
+
+//---------- このjsファイルが読み込まれた瞬間に発動 予測変換を設定する部分 ----------//
+(async () => {// 干渉しないように 即時関数
+  const stationList = await get(`${url}?list=station`);
+
+  const stationDatalist = document.getElementById('stationList');
+  console.log(stationList.main);
+
+  for (const stationData of stationList.main) {
+    stationDatalist.insertAdjacentHTML("beforeend", `<option value="${stationData}">`);
+  }
+  console.log(stationList.main);
+
+})();
+//---------- このjsファイルが読み込まれた瞬間に発動 予測変換を設定する部分 ----------//
+
+
+//----------  util 群 ----------//
+
+function get(url) {// Get リクエスト
+  return new Promise((resolve) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => resolve(data))
+  });
+}
+
+//----------  util 群 ----------//
